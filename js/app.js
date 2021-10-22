471adc74e6ca8ecba7dfcd6f0689995fd84757f1
james-muriithi <muriithijames556@gmail.com>
2021-10-22 09:42:20 +0300

    append menu using js

---
+++ js/app.js
@@ -0,0 +1,63 @@
+/**
+ * Business Logic
+ */
+function Burger(name, image, newBurger=false) {
+  this.name = name;
+  this.price = 0;
+  this.image = image;
+  this.toppings = [];
+  this.crust = null;
+  this.newBurger =  newBurger;
+}
+
+const burgers = [
+  new Burger("Beef Burger", "./images/burger1.png", true),
+  new Burger("Spicy Tandoori", "./images/burger2.png"),
+  new Burger("Chhese Burger", "./images/burger3.png"),
+  new Burger("Buffalo Burger", "./images/burger4.png"),
+  new Burger("BBQ Chcken", "./images/burger5.png"),
+  new Burger("Crispy Chicken", "./images/burger6.png"),
+];
+
+/**
+ * Frontend logic
+ */
+
+$(function () {
+  // append burgers to menu
+  $("#menu .menu").html('');
+  burgers.forEach((burger) => {
+    $("#menu .menu")
+      .append(`<div class="col-sm-6 col-md-6 col-xl-4 col-xxl-3 mb-5">
+    <div class="menu-card p-3 p-lg-4">
+        <div class="ribbon text-center ${burger.newBurger ? '': 'd-none'}">
+            New
+        </div>
+        <div class="text-center">
+            <img src="${burger.image}" alt="" class="menu-image img-fluid">
+        </div>
+        <div class="card-details text-center pt-3">
+            <h3 class="menu-title fw-bolder">
+                ${burger.name}
+            </h3>
+            <p class="menu-description pt-2">
+                Lorem ipsum dolor sit amet, con sectetur adipiscing elit. Erat mo
+                rbi viverra bibendum in sit. Ac semper arcu facilisis ornare
+            </p>
+        </div>
+        <div class="price-cart d-flex mt-5 align-items-center">
+            <div class="col-6 price fw-bold">
+                Ksh. <span>1200</span>
+            </div>
+            <div class="col-6 d-flex">
+                <span class="add-to-cart d-flex align-items-center ms-auto">
+                    <button class="btn" title="add to cart">
+                        <img src="./images/shopping-cart.png" alt="cart">
+                    </button>
+                </span>
+            </div>
+        </div>
+    </div>
+</div>`);
+  });
+});
